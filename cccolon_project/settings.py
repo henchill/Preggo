283af15cb8541c4737c2828c5485edee8ff487cc
Happy-Ubuntu12 <henchill@mit.edu>
2014-04-17 02:31:19 -0400

    some changes

--- cccolon_project/settings.py
+++ cccolon_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 import dj_database_url
 
+DEVELOPMENT = True
 ALLOWED_HOSTS = ['*']
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -63,10 +64,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-if not DEVELOPMENT:
-    DATABASES['default'] = dj_database_url.config()
-    DEBUG = False
     
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -91,7 +88,7 @@ TEMPLATE_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'#os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
@@ -104,4 +101,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/preggo/login/'
 
-
+if not DEVELOPMENT:
+    DATABASES['default'] = dj_database_url.config()
+    DEBUG = False
