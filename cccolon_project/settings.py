4dde9c1a339be56e817e4c7b68340e02d101dd1e
Happy Enchill <henchill@mit.edu>
2014-04-17 00:19:15 -0400

    added modified medfacts

--- cccolon_project/settings.py
+++ cccolon_project/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Internationalization
